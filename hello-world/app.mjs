f6d3fc06bee75c817be697bd253e6b16c0e7378d
moritalous <moritalous@gmail.com>
2024-09-23 13:53:43 +0000

    update

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,23 +1,63 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
 
-export const lambdaHandler = async (event, context) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      })
-    };
+import { Readability } from '@mozilla/readability';
+import { JSDOM, VirtualConsole } from 'jsdom';
+import { chromium } from 'playwright';
+import TurndownService from 'turndown';
 
-    return response;
+export const lambdaHandler = async (event, lambdaContext) => {
+
+  const url = event.url;
+  const userAgent = event.userAgent;
+
+  const browser = await chromium.launch({
+    args: [
+      "--single-process",
+      "--no-zygote",
+      "--no-sandbox",
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--headless=new",
+    ]
+  });
+  
+  const context = await browser.newContext({
+    userAgent: userAgent
+  });
+
+  const page = await context.newPage();
+  const pageResponse = await page.goto(url);
+
+  const html = await page.content();
+  const status = pageResponse.status();
+
+  browser.close();
+
+  const response = {
+    statusCode: status
   };
+
+  if (pageResponse.ok()) {
+
+    // https://stackoverflow.com/questions/69906136/console-error-error-could-not-parse-css-stylesheet
+    const virtualConsole = new VirtualConsole();
+    virtualConsole.on("error", () => {
+      // No-op to skip console errors.
+    });
+
+    const doc = new JSDOM(html, { virtualConsole });
+
+    const reader = new Readability(doc.window.document);
+    const article = reader.parse();
+
+    const turndownService = new TurndownService();
+    const markdown = turndownService.turndown(article.content);
+
+    response.body = JSON.stringify({
+      title: article.title,
+      markdown: markdown
+    });
+  }
+
+  return response;
+
+};
